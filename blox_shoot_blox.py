35eb3ef45dd168cb33d227e8798c3ad0aaa3ff1c
Tanpisit Sangsong <6610110126@email.psu.ac.th>
2024-01-14 18:40:52 +0700

    add_spaceship

--- blox_shoot_blox.py
+++ blox_shoot_blox.py
@@ -5,3 +5,19 @@ from kivy.core.window import Window
 from kivy.uix.label import Label
 from random import randint
 from kivy.clock import Clock
+
+class SpaceShip(Widget):
+    def __init__(self, **kwargs):
+        super(SpaceShip, self).__init__(**kwargs)
+        self.image = Image(source='C://Users//TUF RTX 4060//Desktop//Project//—Pngtree—cartoon airplane model_2652515.png')
+        self.add_widget(self.image)
+
+    def move(self, direction):
+        if direction == 'up' and self.y < Window.height - 100:
+            self.y += 10
+        elif direction == 'down' and self.y > 0:
+            self.y -= 10
+        elif direction == 'right' and self.x < Window.width - 100:
+            self.x += 10
+        elif direction == 'left' and self.x > 0:
+            self.x -= 10
