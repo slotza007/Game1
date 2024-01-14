8782150fa1eb07783f97b3e831c6ac13a88e87c5
Tanpisit Sangsong <6610110126@email.psu.ac.th>
2024-01-14 18:38:40 +0700

    add_import

---
+++ blox_shoot_blox.py
@@ -0,0 +1,7 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.image import Image
+from kivy.core.window import Window
+from kivy.uix.label import Label
+from random import randint
+from kivy.clock import Clock
