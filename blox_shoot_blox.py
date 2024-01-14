a4aacc61aaffa38a0be3e0347d5d6d29104b9773
Tanpisit Sangsong <6610110126@email.psu.ac.th>
2024-01-14 18:43:10 +0700

    add_bullet

--- blox_shoot_blox.py
+++ blox_shoot_blox.py
@@ -21,3 +21,15 @@ class SpaceShip(Widget):
             self.x += 10
         elif direction == 'left' and self.x > 0:
             self.x -= 10
+
+class Bullet(Image):
+    def __init__(self, spaceship, **kwargs):
+        super(Bullet, self).__init__(**kwargs)
+        self.source = 'C://Users//TUF RTX 4060//Desktop//Project//bullet.jpg'
+        self.pos_hint = {'center_x': spaceship.center_x, 'center_y': spaceship.top}
+        Clock.schedule_interval(self.update, 1.0 / 60.0)
+
+    def update(self, *args):
+        self.y += 10
+        if self.top > Window.height:
+            self.parent.remove_widget(self)
